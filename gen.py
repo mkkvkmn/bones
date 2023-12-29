77ad12b8c3e4eb563cd488e01d7c0845e68f6c0c
mkkvkmn <github@feellike.io>
2023-12-29 09:37:15 +0200

    refactor build

--- gen.py
+++ gen.py
@@ -1,3 +1,5 @@
+# source venv/bin/activate
+
 import os
 import re
 import markdown
@@ -11,6 +13,14 @@ from jinja2 import Environment, FileSystemLoader
 # logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+# Global site properties
+SITE_PROPERTIES = {
+    "site_name": "My Awesome Blog",
+    "site_description": "A blog about awesome things.",
+    # Add more global properties as needed
+}
+
+
 # configurations
 OUTPUT_DIR = '_public'
 POSTS_DIR = 'posts'
@@ -23,6 +33,7 @@ ARCHIVE_URL = 'a'
 # Initialize Jinja environment once and load templates
 env = Environment(loader=FileSystemLoader(TEMPLATES_DIR))
 post_template = env.get_template(TEMPLATE_FOR_POST)
+
 
 def parse_front_matter(file_content):
     pattern = re.compile(r'^---\s+(.*?)\s+---\s+(.*)', re.DOTALL)
@@ -38,30 +49,30 @@ def parse_front_matter(file_content):
 def markdown_to_html(md_content):
     return markdown.markdown(md_content)
 
+
 def apply_template(front_matter, html_content):
     return post_template.render(post=front_matter, content=html_content)
 
-def is_outdated(source_path, output_path, template_mod_time):
+
+def is_outdated(source_path, output_path, last_template_mod_time):
     if not os.path.exists(output_path):
         return True
     source_mod_time = os.path.getmtime(source_path)
     output_mod_time = os.path.getmtime(output_path)
-    return source_mod_time > output_mod_time or template_mod_time > output_mod_time
+    return source_mod_time > output_mod_time or last_template_mod_time > output_mod_time
 
 
-def get_metadata():
-    posts_metadata = []
+def get_meta():
+    posts_meta = []
     url_set = set()
-    latest_post_mod_time = 0
+    last_post_mod_time = 0
 
-    template_mod_time = max(os.path.getmtime(f.path)
-                        for f in os.scandir(TEMPLATES_DIR)
-                        if f.is_file())
+    last_template_mod_time = max(os.path.getmtime(f.path) for f in os.scandir(TEMPLATES_DIR) if f.is_file())
 
     for md_file in os.scandir(POSTS_DIR):
         if md_file.name.endswith('.md'):
             post_mod_time = os.path.getmtime(md_file.path)
-            latest_post_mod_time = max(latest_post_mod_time, post_mod_time)
+            last_post_mod_time = max(last_post_mod_time, post_mod_time)
 
             with open(md_file.path, 'r') as file:
                 file_content = file.read()
@@ -74,21 +85,25 @@ def get_metadata():
                 raise ValueError(f"Duplicate URL '{url}' found in {md_file.name}")
             url_set.add(url)
 
-            posts_metadata.append({
+            posts_meta.append({
                 'title': front_matter.get('title', ''),
                 'excerpt': front_matter.get('excerpt', ''),
                 'date': front_matter.get('date', ''),
                 'url': url
+                ,'filename': md_file.name
             })
 
-    return posts_metadata, latest_post_mod_time, template_mod_time
+    latest_post = max(posts_meta, key=lambda x: x['date'])
 
+    meta = {
+        'last_post_mod_time':last_post_mod_time,
+        'last_template_mod_time':last_template_mod_time,
+        'posts':posts_meta,
+        'latest_post':latest_post,
+        'site':SITE_PROPERTIES,
+    }
 
-def get_latest_post_metadata(posts_metadata):
-    if posts_metadata:
-        # print(max(posts_metadata, key=lambda x: x['date']))
-        return max(posts_metadata, key=lambda x: x['date'])
-    return None
+    return meta
 
 
 def setup_output_directory(full_rebuild):
@@ -101,64 +116,50 @@ def setup_output_directory(full_rebuild):
         os.makedirs(OUTPUT_DIR)
 
 
-def build_landing(posts_metadata, template_mod_time, latest_post_mod_time, full_rebuild=False):
-    landing_output_path = os.path.join(OUTPUT_DIR, 'index.html')
-
-    landing_needs_rebuild = (
+def build_page(template_name, output_path, meta, full_rebuild=False):
+    needs_rebuild = (
         full_rebuild or 
-        not os.path.exists(landing_output_path) or 
-        os.path.getmtime(landing_output_path) < latest_post_mod_time or 
-        os.path.getmtime(landing_output_path) < template_mod_time
+        not os.path.exists(output_path) or 
+        os.path.getmtime(output_path) < meta['last_post_mod_time'] or 
+        os.path.getmtime(output_path) < meta['last_template_mod_time']
     )
+
+    if needs_rebuild:
+        template = env.get_template(template_name)
+        content = template.render(meta=meta)
+        os.makedirs(os.path.dirname(output_path), exist_ok=True)
+        with open(output_path, 'w') as file:
+            file.write(content)
+        logging.info(f"Built: {output_path}")
+
+ 
+def build_landing(meta, full_rebuild=False):
+    output_path = os.path.join(OUTPUT_DIR, 'index.html')
+    build_page(TEMPLATE_FOR_LANDING, output_path, meta, full_rebuild)
+
+
+def build_archive(meta, full_rebuild=False):
+    output_path = os.path.join(OUTPUT_DIR, ARCHIVE_URL, 'index.html')
+    build_page(TEMPLATE_FOR_ARCHIVE, output_path, meta, full_rebuild)
+
     
-    if landing_needs_rebuild:
-        latest_post = get_latest_post_metadata(posts_metadata)
-        landing_template = env.get_template('landing.html')
-        landing_content = landing_template.render(latest_post=latest_post)
-        os.makedirs(OUTPUT_DIR, exist_ok=True)
-        logging.info("Building: Landing Page")
-        with open(landing_output_path, 'w') as file:
-            file.write(landing_content)
+def build_posts(meta, full_rebuild=False):
 
+    for post_meta in meta['posts']:
+        source_path = os.path.join(POSTS_DIR, post_meta['filename'])
 
-def build_archive(posts_metadata, template_mod_time, latest_post_mod_time, full_rebuild=False):
-    archive_output_path = os.path.join(OUTPUT_DIR, ARCHIVE_URL, 'index.html')
-
-    archive_needs_rebuild = (
-        full_rebuild or 
-        not os.path.exists(archive_output_path) or 
-        os.path.getmtime(archive_output_path) < latest_post_mod_time or 
-        os.path.getmtime(archive_output_path) < template_mod_time
-    )
-
-    if archive_needs_rebuild:
-        posts_metadata.sort(key=lambda x: x['date'], reverse=True)
-        archive_template = env.get_template(TEMPLATE_FOR_ARCHIVE)
-        archive_content = archive_template.render(posts=posts_metadata)
-        os.makedirs(os.path.join(OUTPUT_DIR, ARCHIVE_URL), exist_ok=True)
-        logging.info(f"Building: Archive (url: {ARCHIVE_URL})")
-        with open(archive_output_path, 'w') as file:
-            file.write(archive_content)
-
-
-def build_posts(template_mod_time):
-    for md_file in os.scandir(POSTS_DIR):
-        if md_file.name.endswith('.md'):
-            source_path = md_file.path
+        if os.path.exists(source_path):
             with open(source_path, 'r') as file:
                 file_content = file.read()
+
             front_matter, md_content = parse_front_matter(file_content)
             html_content = markdown_to_html(md_content)
+            output_file_path = os.path.join(OUTPUT_DIR, front_matter['url'], 'index.html')
+            needs_rebuild = full_rebuild or is_outdated(source_path, output_file_path, meta['last_template_mod_time'])
 
-            output_dir_path = os.path.join(OUTPUT_DIR, front_matter['url'])
-            os.makedirs(output_dir_path, exist_ok=True)
-            output_file_path = os.path.join(output_dir_path, 'index.html')
-
-            if is_outdated(source_path, output_file_path, template_mod_time):
-                logging.info(f"Building: {front_matter['url']}")
-                final_output = apply_template(front_matter, html_content)
-                with open(output_file_path, 'w') as file:
-                    file.write(final_output)
+            if needs_rebuild:
+                page_meta = {**meta, 'post': front_matter,'content': html_content,}
+                build_page(TEMPLATE_FOR_POST, output_file_path, page_meta)
 
 
 def generate_site(full_rebuild=False):
@@ -167,14 +168,14 @@ def generate_site(full_rebuild=False):
     setup_output_directory(full_rebuild)
 
     try:
-        posts_metadata, latest_post_mod_time, template_mod_time = get_metadata()
+        meta = get_meta()
     except ValueError as e:
         logging.error(e)
         return
 
-    build_landing(posts_metadata, template_mod_time, latest_post_mod_time, full_rebuild)
-    build_archive(posts_metadata, template_mod_time, latest_post_mod_time, full_rebuild)
-    build_posts(template_mod_time)
+    build_landing(meta, full_rebuild)
+    build_archive(meta, full_rebuild)
+    build_posts(meta, full_rebuild)
 
     elapsed_time = time.time() - start_time
     logging.info(f"Done in {elapsed_time:.2f} seconds")
